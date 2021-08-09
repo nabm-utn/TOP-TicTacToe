ebe26f58f677d01e13e7d5684248db051b53b87b
nabm-utn <nabm.utn@gmail.com>
2021-08-09 20:54:21 -0300

    add Player factory

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameboard = (() => {
     let board = Array(9).fill(" ")
     
-    const setOwner = (index, owner) => {
+    const setOwnerName = (index, owner) => {
         board[index] = owner.name
         document.querySelector(`[data-index="${index}"]`).textContent = owner.symbol
     }
@@ -10,8 +10,22 @@ const gameboard = (() => {
         return board[index]
     }
 
-    return {setOwner, getOwnerName}
+    return {setOwnerName, getOwnerName}
 })()
 
-gameboard.setOwner(7, {name:"me"})
-console.log(gameboard.getOwnerName(7))
+const Player = (id, name, symbol) => {
+    const getId = () => id;
+    const getName = () => name;
+    const setName = (newName) => {name = newName};
+    const getSymbol = () => symbol;
+
+    let score = 0
+    const getScore = () => score;
+    const increaseScore = () => {score += 1};
+    
+    return {getId, getName, setName, getSymbol, getScore, increaseScore}
+    }
+
+const player1 = Player("p1", "player 1", "X");
+const player2 = Player("p2", "player 2", "O");
+
