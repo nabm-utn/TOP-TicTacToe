8993b05bb5d9fb489c3de22e524e9af23df7119e
nabm-utn <nabm.utn@gmail.com>
2021-08-10 10:42:23 -0300

    Add basic game and round logic

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
 const gameboard = (() => {
     let board = Array(9).fill(" ")
     
-    const setOwnerName = (index, owner) => {
-        board[index] = owner.name
-        document.querySelector(`[data-index="${index}"]`).textContent = owner.symbol
+    const setOwnerId = (index, owner) => {
+        board[index] = owner.getId();
+        let boardSection = document.querySelector(`[data-index="${index}"]`);
+        boardSection.style.color = owner.getColor();
+        boardSection.textContent = owner.getSymbol();
     }
 
-    const getOwnerName = (index) => {
+    const getOwnerId = (index) => {
         return board[index]
     }
 
-    return {setOwnerName, getOwnerName}
+    return {setOwnerId, getOwnerId}
 })()
 
-const Player = (id, name, symbol) => {
+const Player = (id, name, symbol, color) => {
     const getId = () => id;
     const getName = () => name;
     const setName = (newName) => {name = newName};
     const getSymbol = () => symbol;
+    const getColor = () => color;
+    const setColor = (newColor) => {color = newColor};
 
     let score = 0
     const getScore = () => score;
     const increaseScore = () => {score += 1};
     
-    return {getId, getName, setName, getSymbol, getScore, increaseScore}
+    return {getId, 
+            getName, setName, 
+            getSymbol, 
+            getColor, setColor,
+            getScore, increaseScore}
     }
 
-const player1 = Player("p1", "player 1", "X");
-const player2 = Player("p2", "player 2", "O");
+
+const game = (() => {
+    const player1 = Player("p1", "player 1", "X", "#f00");
+    const player2 = Player("p2", "player 2", "O", "#0f0");
+
+    let turn = 0;
+    let gameFinished = false;
+
+    const getTurn = () => turn;
+    const playRound = (event) => {
+        choice = event.target.dataset.index;
+        if (turn % 2 === 0) {
+            gameboard.setOwnerId(choice, player1);
+        } else {
+            gameboard.setOwnerId(choice, player2);
+        }
+        turn += 1;
+    }
+
+    const sections = document.querySelectorAll(".board-section");
+    sections.forEach((section) => {
+        addEventListener("click", playRound)
+    })
+
+    return {getTurn}
+})()
+
+// TestGround...
+
+
 
